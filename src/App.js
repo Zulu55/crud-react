3a36ec45c9fceed01dc7e624556e402b74e334b9
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-11-24 15:49:20 -0500

    Update and Delete Tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { isEmpty, size } from 'lodash'
-import { addDocument, getCollection } from './actions'
+import { addDocument, deleteDocument, getCollection, updateDocument } from './actions'
 
 function App() {
   const [task, setTask] = useState("")
@@ -47,7 +47,13 @@ function App() {
     setTask("")
   }
 
-  const deleteTask = (id) => {
+  const deleteTask = async(id) => {
+    const result = await deleteDocument("tasks", id)
+    if (!result.statusResponse) {
+      setError(result.error)
+      return
+    }
+    
     const filteredTasks = tasks.filter(task => task.id !== id)
     setTasks(filteredTasks)
   }
@@ -58,10 +64,16 @@ function App() {
     setTask(myTask.name)
   }
 
-  const saveTask = (e) => {
+  const saveTask = async(e) => {
     e.preventDefault()
 
     if (!validForm()) {
+      return
+    }
+
+    const result = await updateDocument("tasks", id, { name: task }) 
+    if (!result.statusResponse) {
+      setError(result.error)
       return
     }
 
--- src/actions.js
+++ src/actions.js
@@ -40,3 +40,53 @@ export const addDocument = async(collection, data) => {
 
     return result
 }
+
+export const getDocument = async(collection, id) => {
+    const result = {
+        statusResponse: false,
+        data: null,
+        error: null
+    }
+
+    try {
+        const response = await db.collection(collection).doc(id).get()
+        result.data = { id: response.id, ...response.data() }
+        result.statusResponse = true
+    } catch (error) {
+        result.error = error
+    }
+
+    return result
+}
+
+export const updateDocument = async(collection, id, data) => {
+    const result = {
+        statusResponse: false,
+        error: null
+    }
+
+    try {
+        await db.collection(collection).doc(id).update(data)
+        result.statusResponse = true
+    } catch (error) {
+        result.error = error
+    }
+
+    return result
+}
+
+export const deleteDocument = async(collection, id) => {
+    const result = {
+        statusResponse: false,
+        error: null
+    }
+
+    try {
+        await db.collection(collection).doc(id).delete()
+        result.statusResponse = true
+    } catch (error) {
+        result.error = error
+    }
+
+    return result
+}
